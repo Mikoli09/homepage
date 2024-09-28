9e5a3e76a44dfa8fd2f72de26297e9fc4c210818
Marcin Osojca <osojca.marcin1@gmail.com>
2024-09-28 16:39:20 +0200

    JS functions added

--- js/script.js
+++ js/script.js
@@ -1,70 +1,83 @@
-console.log("Cześć");
+{
+    console.log("Cześć");
 
-let body = document.querySelector("body");
-let backgroundChangeButton_Element = document.querySelector(".js-backgroundChangeButton");
-
-let amountText = document.getElementById("amountText");
-let amount = 0;
-let verifyButton_Element = document.querySelector(".js-verifyButton");
-
-let tab = [
-    document.getElementById("Status1"), //0
-    document.getElementById("Status2"), //1
-    document.getElementById("Status3"), //2
-    document.getElementById("Status4"), //3
-    document.getElementById("Status5"), //4
-    document.getElementById("Status6"), //5
-    document.getElementById("Status7") //6
-];
+    const backgroundChangeButton_Element = document.querySelector(".js-backgroundChangeButton");
+    let amount = 0;
 
 
-// Switch background color by button and change button inner text
-backgroundChangeButton_Element.addEventListener("click", () => {
 
-    body.classList.toggle("body--darkMode")
-    backgroundChangeButton_Element.innerText = body.classList.contains("body--darkMode") ?
-        "Wyłącz ciemne tło" : "Włącz ciemne tło";
+    const changeBackgroundColor = () => {
+        let body = document.querySelector("body");
 
-});
+        body.classList.toggle("body--darkMode")
+        backgroundChangeButton_Element.innerText = body.classList.contains("body--darkMode") ?
+            "Wyłącz ciemne tło" : "Włącz ciemne tło";
 
-// Checkbox check
-tab.forEach(function (StatusX) {
-    StatusX.addEventListener("click", () => {
-        if (StatusX.checked) {
-            amount++;
+    };
+
+    const verifyCheckbox = () => {
+        const checkboxArray = [
+            document.getElementById("status1"), //0
+            document.getElementById("status2"), //1
+            document.getElementById("status3"), //2
+            document.getElementById("status4"), //3
+            document.getElementById("status5"), //4
+            document.getElementById("status6"), //5
+            document.getElementById("status7")  //6
+        ];
+
+        checkboxArray.forEach(element => {
+            element.addEventListener("click", () => {
+                if (element.checked) {
+                    amount++;
+                }
+                else {
+                    amount--;
+                }
+            })
+        });
+    };
+
+    verifyCheckbox();
+
+    const checkboxStatusTextUpdate = () => {
+        let amountText = document.getElementById("amountText");
+
+        switch (amount) {
+            case 0:
+                return amountText.innerText = "zero książek";
+
+            case 1:
+                return amountText.innerText = "jedną książkę";
+
+            case 2:
+                return amountText.innerText = "dwie książki";
+
+            case 3:
+                return amountText.innerText = "trzy książki";
+
+            case 4:
+                return amountText.innerText = "cztery książki";
+
+            case 5:
+                return amountText.innerText = "pięć książek";
+
+            case 6:
+                return amountText.innerText = "sześć książek";
+
+            case 7:
+                return amountText.innerText = "wszystkie książki";
         }
-        else {
-            amount--;
-        }
-    });
-});
+    };
 
 
-verifyButton_Element.addEventListener("click", () => {
-    switch (amount) {
-        case 0:
-            amountText.innerText = "zero książek";
-            break;
-        case 1:
-            amountText.innerText = "jedną książkę";
-            break;
-        case 2:
-            amountText.innerText = "dwie książki";
-            break;
-        case 3:
-            amountText.innerText = "trzy książki";
-            break;
-        case 4:
-            amountText.innerText = "cztery książki";
-            break;
-        case 5:
-            amountText.innerText = "pięć książek";
-            break;
-        case 6:
-            amountText.innerText = "sześć książek";
-            break;
-        case 7:
-            amountText.innerText = "wszystkie książki";
-            break;
-    }
-});
+    const init = () => {
+        const verifyButton_Element = document.querySelector(".js-verifyButton");
+
+        verifyButton_Element.addEventListener("click", checkboxStatusTextUpdate);
+        backgroundChangeButton_Element.addEventListener("click", changeBackgroundColor);
+    };
+
+    init();
+
+}
